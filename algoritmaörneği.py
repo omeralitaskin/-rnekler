66a10e342c435a4c618e294bbfaedb05d41e2fa7
omeralitaskin <71253475+omeralitaskin@users.noreply.github.com>
2020-12-19 14:50:34 +0300

    Add files via upload
    while döngüsü ile sayıların tek yada çift olduğunu bulan kod örneği.

---
+++ algoritmaörneği.py
@@ -0,0 +1,12 @@
+
+
+y = """programa hoş geldiniz..."""
+
+print(y)
+while True:
+    x = int(input("Lütfen bir sayı giriniz: "))
+    if x %2==0:
+        print("Sayı çifttir")
+    elif x %2==1:
+        print("Tek sayıdır")
+    
